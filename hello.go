79c718317ab51f5d67b52edd81f4fbc969ebbe65
George Charles <gkcharles@outlook.com>
2024-08-30 22:02:24 +0200

    add changes

--- hello.go
+++ hello.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
 
 func main(){
-	fmt.Println("Hello, world.")
-}
 
+	conn, err := pgx.Connect(context.Background(), os.Getenv("postgres://george:LAGos123@@localhost:5432/george"))
+	if err != nil {
+		fmt.Println("There is an error")
+	}
+
+	defer conn.Close(context.Background())
+
+	var greeting string 
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Println("error is zero....")
+	}
+	fmt.Println(greeting)
+}
